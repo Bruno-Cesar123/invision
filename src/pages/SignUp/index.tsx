666818e61137c1afa6f8a73f2dad2f7afefa1f46
Bruno Cesar <brunocesar12386@gmail.com>
2021-09-25 23:19:22 -0300

    create page sign up

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,143 @@
+import { Link, useHistory } from 'react-router-dom';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
+import { toast } from 'react-toastify';
 
-import { Container } from './styles';
+import Button from '../../components/Button';
+import ButtonGoogle from '../../components/ButtonGoogle';
+import Input from '../../components/Input';
+
+
+import { Container, Header, Form, Actions, Info } from './styles';
+import Background from '../../components/Background';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required('*Este campo não pode ser vazio'),
+  email: Yup.string()
+    .required('*Este campo não pode ser vazio')
+    .email('Email incorreto'),
+  password: Yup.string().min(6, '*No mínimo 6 caracteres').required('*Este campo não pode ser vazio'),
+});
 
 export default function SignUp() {
+  const history = useHistory();
+
+  function SignUpWithGoogle() {
+    try {
+      history.push('/');
+      toast.success('Cadastro com google realizado com sucesso');
+    } catch(err) {
+      toast.error('Não foi possível realizar o cadastro com google');
+    }
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit:(data: SignUpFormData) => {
+      try {
+        history.push('/');
+        toast.success('cadastro realizado com sucesso');
+      } catch(err) {
+        toast.error('Não foi possícel realizar o cadastro');
+      }
+    }
+  })
+
   return (
     <Container>
-      <h1>sign up</h1>
+      <Background />
+      <section>
+        <Header>
+          <h3>Invision</h3>
+        </Header>
+
+        <Form onSubmit={formik.handleSubmit}>
+          <h1>Getting Started</h1>
+
+          <label htmlFor="name">Full name
+            <Input
+              type="text" 
+              id="name"
+              name="name"
+              title="name"
+              aria-label="Input de nome"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              className={`${formik.touched.name && Boolean(formik.errors.name) ? 'error': ''}`}
+            />
+            <p>{formik.touched.name && formik.errors.name}</p>
+          </label>
+
+
+          <label htmlFor="email">Users name or email
+            <Input
+              type="text" 
+              id="email"
+              name="email"
+              title="email"
+              aria-label="Input de email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className={`${formik.touched.email && Boolean(formik.errors.email) ? 'error': ''}`}
+            />
+            <p>{formik.touched.email && formik.errors.email}</p>
+          </label>
+          
+
+          <label htmlFor="password">Password
+            <Input 
+              type="password" 
+              id="password"
+              title="password"
+              name="password"
+              aria-label="Input de senha"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              className={`${formik.touched.password && Boolean(formik.errors.password) ? 'error': ''}`}
+            />
+            <p>{formik.touched.password && formik.errors.password}</p>
+          </label>
+
+          <Button
+            type="submit"
+            title="Sign up"
+          />
+
+          <p>Or</p>
+
+          <ButtonGoogle
+            type="button"
+            title="Sign up with Google"
+            onClick={SignUpWithGoogle}
+          />
+
+          <Info>
+            <p>
+              By signing up, you agree to <span>Invison</span>
+              <div>
+                <Link to="/">Terms of Conditions</Link> and 
+                <Link to="/"> Privacy Police</Link>
+              </div>
+            </p>
+          </Info>
+
+          <Actions>
+              <p>Already on <span>Invision?</span></p>
+              <Link to="/">Log in</Link>
+          </Actions>
+        </Form>
+      </section>
     </Container>
   )
 }
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,133 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.div`
+  height: 100vh;
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+
+  @media (max-width: 936px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+export const Header = styled.header`
+  width: 100%;
+
+  display: flex;
+  justify-content: flex-end;
+  padding: 120px 124px 0;
+
+  h3 {
+    font-size: 48px;
+    color: var(--black);
+  }
+`;
+
+export const Form = styled.form`
+  max-width: 433px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 47px;
+
+  h1 {
+    font-size: 30px;
+    color: var(--gray-500);
+    font-weight: 400;
+    opacity: 1;
+  }
+
+  label {
+    width: 100%;
+    text-align: left;
+    justify-content: left;
+    margin-top: 50px;
+    margin-bottom: 4px;
+    color: var(--gray-500);
+
+    & + label {
+      margin-top: 18px;
+    }
+
+    p {
+      color: red;
+      margin-top: 18px;
+    }
+  }
+
+  > a {
+    margin-top: 18px;
+    text-decoration: none;
+    color: var(--gray-500);
+    text-align: right;
+    width: 100%;
+  }
+
+  > p {
+    width: 80%;
+    text-align: center;
+    margin-top: 40px;
+    position: relative;
+
+    &:before {
+      content: "";
+      border-bottom: 0.5px solid var(--gray-500);
+      position: absolute;
+      width: 50%;
+      top: 50%;
+      left: -18px;
+      opacity: 1;
+    }
+
+    &:after {
+      content: "";
+      border-bottom: 0.5px solid var(--gray-500);
+      width: 50%;
+      position: absolute;
+      top: 50%;
+      right: -18px;
+      opacity: 1;
+    }
+  }
+
+  @media (max-width: 1180px) {
+    padding: 0 12px;
+  }
+`;
+
+export const Actions = styled.div`
+  margin-top: 20px;
+  display: flex;
+  align-items: center;
+
+  span {
+    font-weight: 900;
+  }
+
+  a {
+    color: var(--blue);
+  }
+
+  @media (max-width: 500px) {
+    margin-bottom: 8px;
+  }
+`;
+
+export const Info = styled.div`
+  margin-top: 40px;
+  text-align: center;
+
+  p {
+    span {
+      font-weight: 900;
+    }
+
+    a {
+      color: var(--blue);
+    }
+  }
+`;
